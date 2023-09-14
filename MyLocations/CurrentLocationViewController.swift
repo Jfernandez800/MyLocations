29b45d4e81e8758ce0097b4a7a62d011b25bfca8
James Fernandez <jfernandez800@gmail.com>
2023-09-14 02:29:13 -0400

    -connected Get My Location Button to Touch Up Inside to allow the locations message to display. -add Update Labels Method to format and update the locations data. -created variable and assigned it the CLLocation object from location manager. -added the updateLabels method.

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -5,13 +5,14 @@
 //  Created by James Fernandez on 9/12/23.
 //
 
-import UIKit
 //--------------------------Chapter 22---------------------------------
 import CoreLocation
+import UIKit
+
 
 class CurrentLocationViewController: UIViewController, /*Chapter 22*/ CLLocationManagerDelegate {
     
-    //will give you GPS coordinates.
+    //Chapter 22 - will give you GPS coordinates.
     let locationManager = CLLocationManager()
     
     //--------------------------Chapter 22---------------------------------
@@ -21,38 +22,76 @@ class CurrentLocationViewController: UIViewController, /*Chapter 22*/ CLLocation
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var tagButton: UIButton!
     @IBOutlet weak var getButton: UIButton!
-
+    
+    //Chapter 22 - You will store the user’s current location in this variable.
+    var location: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLabels()
     }
     
     // MARK: - Actions
-    //This method is hooks up to "Get My Location" button. tells the location manager that the view controller is its delegate and that you want to receive locations with an accuracy of up to ten meters.
+    //Chapter 22 - This method is hooks up to "Get My Location" button. tells the location manager that the view controller is its delegate and that you want to receive locations with an accuracy of up to ten meters.
     @IBAction func getLocation() {
         
-        //this checks the current authorization status. If it is .notDetermined — meaning that this app has not asked for permission yet — then the app will request “When In Use” authorization.
+        //Chapter 22 - this checks the current authorization status. If it is .notDetermined — meaning that this app has not asked for permission yet — then the app will request “When In Use” authorization.
         let authStatus = locationManager.authorizationStatus
         if authStatus == .notDetermined {
             locationManager.requestWhenInUseAuthorization()
             return
         }
+        
+        //Chapter 22 - This shows the alert if the authorization status is denied or restricted.
+        if authStatus == .denied || authStatus == .restricted {
+          showLocationServicesDeniedAlert()
+        return
+        }
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        //Then you start the location manager.
+        //Chapter 22 - Then you start the location manager.
         locationManager.startUpdatingLocation()
     }
     
     // MARK: - CLLocationManagerDelegate
-    //error.localizedDescription bit which, instead of simply printing out the contents of the error variable,
+    //Chapter 22 - error.localizedDescription bit which, instead of simply printing out the contents of the error variable,
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
-        //outputs a human understandable version of the error (if possible) based on the device’s current locale, or language setting.
+        //Chapter 22 - outputs a human understandable version of the error (if possible) based on the device’s current locale, or language setting.
         print("didFailWithError \(error.localizedDescription)")
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
         let newLocation = locations.last!
         print("didUpdateLocations \(newLocation)")
+        
+        //Chapter 22 - You store the CLLocation object that you get from the location manager into the instance variable and call a new updateLabels() method.
+        location = newLocation
+        updateLabels()
+    }
+    
+    // MARK: - Helper Methods
+    //Chapter 22 - This pops up an alert with a helpful hint.
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Please enable location services for this app in Settings.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //Chapter 22 - If there is a valid location object, you convert the latitude and longitude, which are values with type Double, into strings and put them into the labels.
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to Start"
+        }
     }
 }
-
