6541ef6b4be0c580122ce44947233b6ff80355a7
James Fernandez <jfernandez800@gmail.com>
2023-09-15 00:34:27 -0400

    -Started Ch 23 - Replaced functions locationManager -Added Functions stopLocationManager & updateLabels -stopLocationManager - checks whether the boolean instance variable updatingLocation is true or false. -updateLabels - added statements to pick which message to update messageLabel.

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -26,6 +26,10 @@ class CurrentLocationViewController: UIViewController, /*Chapter 22*/ CLLocation
     //Chapter 22 - You will store the user’s current location in this variable.
     var location: CLLocation?
     
+    //Chapter 23 -
+    var updatingLocation = false
+    var lastLocationError: Error?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabels()
@@ -44,8 +48,8 @@ class CurrentLocationViewController: UIViewController, /*Chapter 22*/ CLLocation
         
         //Chapter 22 - This shows the alert if the authorization status is denied or restricted.
         if authStatus == .denied || authStatus == .restricted {
-          showLocationServicesDeniedAlert()
-        return
+            showLocationServicesDeniedAlert()
+            return
         }
         
         locationManager.delegate = self
@@ -56,9 +60,34 @@ class CurrentLocationViewController: UIViewController, /*Chapter 22*/ CLLocation
     
     // MARK: - CLLocationManagerDelegate
     //Chapter 22 - error.localizedDescription bit which, instead of simply printing out the contents of the error variable,
+    //    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
+    //Chapter 22 - outputs a human understandable version of the error (if possible) based on the device’s current locale, or language setting.
+    //        print("didFailWithError \(error.localizedDescription)")
+    //    }
+    
+    //Chapter 23 -
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
-        //Chapter 22 - outputs a human understandable version of the error (if possible) based on the device’s current locale, or language setting.
         print("didFailWithError \(error.localizedDescription)")
+        //Chapter 23 - The CLError.locationUnknown error means the location manager was unable to obtain a location right now.
+        if (error as NSError).code == CLError.locationUnknown.rawValue {
+            return
+        }
+        /*Chapter 23 - In the case of a more serious error, you store the error object into the new instance variable, lastLocationError.
+        That way, you can look up later what kind of error you were dealing with.*/
+        lastLocationError = error
+        //Chapter 23 - If obtaining a location appears to be impossible for wherever the user currently is on the globe, then you need to tell the location manager to stop.
+        stopLocationManager()
+        updateLabels()
+    }
+    
+    //Chapter 23 - Checks whether the boolean instance variable updatingLocation is true or false. 
+    func stopLocationManager() {
+        //Chapter 23 - The reason for having this updatingLocation variable is that you are going to change the appearance of the Get My Location button and the status message label when the app is trying to obtain a location fix, to let the user know the app is working on it.
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
@@ -91,7 +120,24 @@ class CurrentLocationViewController: UIViewController, /*Chapter 22*/ CLLocation
             longitudeLabel.text = ""
             addressLabel.text = ""
             tagButton.isHidden = true
-            messageLabel.text = "Tap 'Get My Location' to Start"
+            //Chapter 23 - Remove the following line
+            //Chapter 23 - messageLabel.text = "Tap 'Get My Location' to Start"
+            //Chapter 23 - The new code starts here: The new code determines what to put in the messageLabel at the top of the screen.
+            let statusMessage: String
+            if let error = lastLocationError as NSError? {
+                if error.domain == kCLErrorDomain && error.code == CLError.denied.rawValue {
+                    statusMessage = "Location Services Disabled"
+                } else {
+                    statusMessage = "Error Getting Location"
+                }
+            } else if !CLLocationManager.locationServicesEnabled() {
+                statusMessage = "Location Services Disabled"
+            } else if updatingLocation {
+                statusMessage = "Searching..."
+            } else {
+                statusMessage = "Tap 'Get My Location' to Start"
+            }
+            messageLabel.text = statusMessage
         }
     }
 }
